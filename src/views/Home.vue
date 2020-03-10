4b272d45691af98dce4fcb1e534d28dd3afb6c76
Echo127 <echo127@gmail.com>
2020-03-10 13:48:47 +1300

    add basic router and images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
+/* eslint-disable prettier/prettier */
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <div
+        v-for="destination in destinations"
+        :key="destination.name"
+      >
+        <router-link :to="destination.slug">
+          {{ destination.name }}
+        </router-link>
+        <figure>
+          <router-link :to="destination.name">
+            <img
+              :src="require(`@/assets/${destination.image}`)"
+              :alt="destination.image"
+            />
+          </router-link>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import store from "../store";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      destinations: store.destinations
+    };
   }
 };
 </script>
+<style scoped>
+.home {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+</style>
